55d9ca0ce969ad8471fa37cc84950a5fb7e4e0bb
estein1988 <estein1988@gmail.com>
2020-12-31 10:42:51 -0700

    Make app component functional and add counter.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
 
-    </div>
-  );
+  state = {
+    count: 0
+  }
+
+  incrementCount = () => {
+    this.setState({count: this.state.count + 1})
+  }
+
+  decrementCount = () => {
+    this.setState({count: this.state.count - 1})
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>{this.state.count}</h1>
+        <button onClick={this.incrementCount}>+</button>
+        <button onClick={this.decrementCount}>-</button>
+      </div>
+      );
+  }
 }
 
 export default App;
