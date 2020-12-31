30787439801086cd11f36474db794837b1abafcc
estein1988 <estein1988@gmail.com>
2020-12-31 11:06:08 -0700

    Change to functional component and manipulate state using the useState hook.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
-import React, {Component} from 'react'
+import React, {useState} from 'react'
 import './App.css';
 
-class App extends Component {
+function App() {
 
-  state = {
-    count: 0
-  }
+  const [count, setCount] = useState(0)
 
-  incrementCount = () => {
-    this.setState({count: this.state.count + 1})
-  }
-
-  decrementCount = () => {
-    this.setState({count: this.state.count - 1})
-  }
-
-  render(){
     return (
       <div className="App">
-        <h1>{this.state.count}</h1>
-        <button onClick={this.incrementCount}>+</button>
-        <button onClick={this.decrementCount}>-</button>
+        <h1>{count}</h1>
+        <button onClick={() => setCount(count + 1)}>+</button>
+        <button onClick={() => setCount(count - 1)}>-</button>
       </div>
       );
-  }
 }
 
 export default App;
